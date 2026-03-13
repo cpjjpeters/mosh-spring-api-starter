0e68301a6ce534d60249d49d677bef9a5360f57c
cpjjpeters <cpjjpeters@gmail.com>
2026-03-13 07:50:56 +0100

    use name dynamically

--- src/main/java/com/codewithmosh/store/controller/HomeController.java
+++ src/main/java/com/codewithmosh/store/controller/HomeController.java
@@ -1,17 +1,16 @@
 package com.codewithmosh.store.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping("/")
-    public String index() {
-        return "index.html";
+    public String index(Model model) {
+        model.addAttribute("name", "Mosh");
+        return "index";
     }
-    @RequestMapping("/hello")
-    public String sayHello() {
-        return "index.html";
-    }
+
 }
