4d5e52476be124c80150ce2821e86480f22570e4
cpjjpeters <cpjjpeters@gmail.com>
2026-03-13 07:30:36 +0100

    incl HomeController, works in pod on p8484

--- src/main/java/com/codewithmosh/store/entities/User.java
+++ src/main/java/com/codewithmosh/store/entities/User.java
@@ -30,40 +30,40 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
-    @Builder.Default
-    private List<Address> addresses = new ArrayList<>();
-
-    public void addAddress(Address address) {
-        addresses.add(address);
-        address.setUser(this);
-    }
-
-    public void removeAddress(Address address) {
-        addresses.remove(address);
-        address.setUser(null);
-    }
-
-    @OneToOne(mappedBy = "user", cascade = CascadeType.REMOVE)
-    private Profile profile;
-
-    @ManyToMany
-    @JoinTable(
-        name = "wishlist",
-        joinColumns = @JoinColumn(name = "user_id"),
-        inverseJoinColumns = @JoinColumn(name = "product_id")
-    )
-    private Set<Product> favoriteProducts = new HashSet<>();
-
-    public void addFavoriteProduct(Product product) {
-        favoriteProducts.add(product);
-    }
-
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + "(" +
-                "id = " + id + ", " +
-                "name = " + name + ", " +
-                "email = " + email + ")";
-    }
+//    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
+//    @Builder.Default
+//    private List<Address> addresses = new ArrayList<>();
+//
+//    public void addAddress(Address address) {
+//        addresses.add(address);
+//        address.setUser(this);
+//    }
+//
+//    public void removeAddress(Address address) {
+//        addresses.remove(address);
+//        address.setUser(null);
+//    }
+//
+//    @OneToOne(mappedBy = "user", cascade = CascadeType.REMOVE)
+//    private Profile profile;
+//
+//    @ManyToMany
+//    @JoinTable(
+//        name = "wishlist",
+//        joinColumns = @JoinColumn(name = "user_id"),
+//        inverseJoinColumns = @JoinColumn(name = "product_id")
+//    )
+//    private Set<Product> favoriteProducts = new HashSet<>();
+//
+//    public void addFavoriteProduct(Product product) {
+//        favoriteProducts.add(product);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return getClass().getSimpleName() + "(" +
+//                "id = " + id + ", " +
+//                "name = " + name + ", " +
+//                "email = " + email + ")";
+//    }
 }
