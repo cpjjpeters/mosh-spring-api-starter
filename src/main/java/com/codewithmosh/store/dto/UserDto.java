e97b6388d6ba1719f7d059402be5ec7387247eb9
cpjjpeters <cpjjpeters@gmail.com>
2026-03-15 19:27:10 +0100

    special json Mapping

--- src/main/java/com/codewithmosh/store/dto/UserDto.java
+++ src/main/java/com/codewithmosh/store/dto/UserDto.java
@@ -1,17 +1,33 @@
 package com.codewithmosh.store.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
 public class UserDto {
     private Long id;
     private String name;
     private String email;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
 
-    public UserDto(Long id, String name, String email) {
+    public UserDto(Long id, String name, String email, LocalDateTime createdAt) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.createdAt = createdAt;
     }
 
     public UserDto() {
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 
     public String getEmail() {
--- src/main/java/com/codewithmosh/store/mapper/UserMapper.java
+++ src/main/java/com/codewithmosh/store/mapper/UserMapper.java
@@ -3,8 +3,11 @@ package com.codewithmosh.store.mapper;
 import com.codewithmosh.store.dto.UserDto;
 import com.codewithmosh.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")  // or localDAteTime Instant
     UserDto toDto(User user);
 }
+//
